1f39bdf3fa4973ddf956720ce02f438a1af38736
Jared <Paubelj45@gmail.com>
2021-09-29 11:06:45 -0500

    Refactored the main program to decrease redundancy and to improve the user input in terminal. Program now outputs final score with correlated amount of cakes.

--- main.py
+++ main.py
@@ -9,66 +9,37 @@ if correct, output correct and ask if try again
 ToDo:
     * Fix display numbers [*****] where guessing number duplicates don't exist.
     * If the number of guessed numbers is 5, then program completes.
-
 """
 rand_list = []
 display_list = []
-count = 0
+cake = ''
+score = 0
 
+# Generate random list
 while len(rand_list) < 5:
-    rand_int = random.randint(0, 9)
+    rand_int = str(random.randint(0, 9))
     if rand_int not in rand_list:
         rand_list.append(rand_int)
 
-while True:
-    try:
-        user_in = int(input('Enter a guessing number.\n >>> '))
-    except ValueError:
-        print("You must enter a number.\n >>> ")
-        continue
-
+# Begin game
+for i in range(5):
+    user_in = input('Enter a guessing number.\n >>> ')
     if user_in in rand_list:
         display_list.append(user_in)
-
         for num in rand_list:
             if num in display_list:
                 print(num, end='')
-            else:
-                print('*', end='')
 
-        print('\nYou got it! 🎂️ Go again? (Y/N)\n >>> ')
-        user_in = str(input())
-        if user_in == 'Y':
-            continue
-        elif user_in == 'N':
-            break
-        else:
-            user_in = str(input("You need to enter (Y/N); Type \'okay\' to continue please. \n >>> "))
-            while True:
-                if user_in == 'okay':
-                    break
-                else:
-                    user_in = str(input("You need to enter (Y/N); Type \'okay\' to continue please. \n >>> "))
-                    continue
+        print('\nYou got it! 🎂️')
+        score += 1
     else:
-        print('\nOh no, that was wrong! Go again? (Y/N)\n >>> ')
-
         for num in rand_list:
-            if num in display_list:
-                print(num, sep='-', end='')
-            else:
-                print('*', sep='-', end='')
+            print('*', end='')
 
-        user_in = str(input())
-        if user_in == 'Y':
-            continue
-        elif user_in == 'N':
-            break
-        else:
-            user_in = str(input("You need to enter (Y/N); Type \'okay\' to continue please."))
-            while True:
-                if user_in == 'okay':
-                    break
-                else:
-                    user_in = str(input("You need to enter (Y/N); Type \'okay\' to continue please."))
-                    continue
+        print('\nOh no, that was wrong!')
+
+for score in range(score + 1):
+    if score > 0:
+        cake += '🎂️'
+
+print(f'Your final score is: { score } { cake }')
