1b84f911885520009760ec08f0316df13a28801c
Jared <Paubelj45@gmail.com>
2021-10-01 10:29:14 -0500

    Updated game loop to account for non-integer inputs (even though the inputs are actually strings), and ensured that the program will cease if the user enters 5 numbers total. The score output has been changed to display the original number.

--- main.py
+++ main.py
@@ -7,41 +7,58 @@ if not in list, output incorrect and if try again
 if correct, output correct and ask if try again
 
 ToDo:
-    * Fix the loop to where the program completes if the display list is 
-        filled up to 5 elements.
-    * Fix issue where user can type anything else other than numbers which 
-        does not compute in guessing program.
+    * Reduce redundancy
+    * Reorganize/refactor code to follow OOP
 """
+
 rand_list = []
 display_list = []
+number_list = str([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 cake = ''
+count = 0
 score = 0
 
 # Generate random list
-while len(rand_list) < 5:
+while count < 5:
     rand_int = str(random.randint(0, 9))
-    if rand_int not in rand_list:
+    if rand_int in rand_list:
+        continue
+    else:
         rand_list.append(rand_int)
+        count += 1
 
+count = 0
 # Begin game
-for i in range(5):
-    user_in = input('Enter a guessing number.\n >>> ')
+while count < 5:
+    user_in = input('\nEnter a guessing number.\n >>> ')
+    if user_in not in number_list:
+        print('You must enter a number')
+        continue
+
     if user_in in rand_list:
         display_list.append(user_in)
         for num in rand_list:
             if num in display_list:
                 print(num, end='')
+            else:
+                print('*', end='')
 
         print('\nYou got it! 🎂️')
+        count += 1
         score += 1
     else:
         for num in rand_list:
-            print('*', end='')
-
+            if num in display_list:
+                print(num, end='')
+            else:
+                print('*', end='')
         print('\nOh no, that was wrong!')
+        count += 1
 
 for score in range(score + 1):
     if score > 0:
         cake += '🎂️'
 
-print(f'Your final score is: { score } { cake }')
+print(f'\nThe number was: ', end='')
+for i in rand_list: print(i, end='')
+print(f'\nYour final score is: {score} {cake}')
